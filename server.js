d0477ae1429de028b6f2cae69bc575b1ba54ff8a
woozizi <c980805@naver.com>
2025-02-24 23:53:12 +0900

    server.js변경

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(4000, () => {
+server.listen(3000, () => {
   console.log("JSON Server is running");
 });
