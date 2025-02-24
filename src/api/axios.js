c38236279c554c2dbaac18a5c16921d16641afd9
woozizi <c980805@naver.com>
2025-02-25 00:12:48 +0900

    변경

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const jsonApi = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://abounding-dandelion-roadway.glitch.me",
 });
 
 export const serverApi = axios.create({
