fd91e2d1f1adae1f085d2885fd5faaedc22a8391
woozizi <c980805@naver.com>
2025-02-25 10:33:52 +0900

    gitignore 수정

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,6 @@ import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./context/AuthContext";
 
-console.log(window.Kakao);
-
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
